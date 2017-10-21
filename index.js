385a6905f4d43a56129fe50abdbb0f836235b02c
Ramesh <gurams@gmail.com>
2017-10-21 21:04:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function destructivelyAppendKitten(name){
   return kittens.push(name)
 }
 function destructivelyPrependKitten(name){
-  return kittens.push(name)
+  return kittens.unshift(name)
 }
 function destructivelyRemoveLastKitten(){
   return kittens.pop()
@@ -19,7 +19,8 @@ function prependKitten(name){
   return [name,...kittens()]
 }
 function removeLastKitten(){
-  return kittens.slice(0,kittens(-1))
+  var newAr= kittens.slice(0,kittens(-1))
+  return newAr
 }
 function removeFirstKitten(){
   return kittens.slice(1)
